56e29823c5dcfebfd692da8bba94c07c129a5aef
ayushpateljs <ayushpateljs@gmail.com>
2026-04-18 02:51:11 +0530

    contact page

--- src/app/features/contact/contact.ts
+++ src/app/features/contact/contact.ts
@@ -1,9 +1,58 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
-export class Contact {}
+export class Contact {
+ infoCards = [
+  {
+    title: 'Currently Available',
+    desc: 'Open for projects & roles',
+    color: 'green'
+  },
+  {
+    title: 'Response Time',
+    desc: 'Within 24 hours',
+    color: 'blue'
+  },
+  {
+    title: 'Works Worldwide',
+    desc: 'Available globally',
+    color: 'purple'
+  }
+];
+
+form = {
+  name: '',
+  email: '',
+  message: ''
+};
+
+sendWhatsApp() {
+
+  if (!this.form.name || !this.form.email) {
+    alert("Please fill required fields");
+    return;
+  }
+
+  const phoneNumber = '918083106697'; // 👉 YOUR NUMBER
+
+  const text =
+`Hello Ayush,
+
+Name: ${this.form.name}
+Email: ${this.form.email}
+
+Message:
+${this.form.message}`;
+
+  const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(text)}`;
+
+  window.open(url, '_blank');
+}
+}
